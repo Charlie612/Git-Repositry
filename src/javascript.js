2c005a10baa7eccb57c0c9a0e120f5fee52630fa
Charlie612 <105488584+Charlie612@users.noreply.github.com>
2022-06-17 14:21:21 +0100

    Updated javascript
    Copy and pasted from sandbox the latest code which includes the integration with the weather API and the conversion to Fahrenheit

--- src/javascript.js
+++ src/javascript.js
@@ -37,10 +37,15 @@ function search(event) {
 
   let h1 = document.querySelector("h1");
   h1.innerHTML = `${searchInput.value}`;
+  //put API call here
+  let apiKey = "a8378d3784950378fd87ca7c671cf2f4";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=metric`;
+
+  axios.get(`${apiUrl}`).then(showTemperature);
 }
 
 let form = document.querySelector("#city-form");
-form.addEventListener("click", search);
+form.addEventListener("submit", search);
 
 // Feature 3
 function changeToFahrenheit(event) {
@@ -60,3 +65,32 @@ temperatureInfahrenheit.addEventListener("click", changeToFahrenheit);
 
 let temperatureInDegrees = document.querySelector("#degrees-celsius");
 temperatureInDegrees.addEventListener("click", changeToCelsius);
+
+//Week 5 challenge
+
+function showTemperature(response) {
+  console.log(response);
+  //console.log(response.data.main.temp);
+  let currentTemperature = Math.round(response.data.main.temp);
+  let displayTemperature = document.querySelector("#temperature");
+  displayTemperature.innerHTML = `${currentTemperature}`;
+  let currentCity = response.data.name;
+  let displayCity = document.querySelector("h1");
+  displayCity.innerHTML = `${currentCity}`;
+}
+
+function showPosition(position) {
+  //console.log(position);
+  //console.log(position.coords.longitude);
+  //console.log(position.coords.latitude);
+  let apiKey = "a8378d3784950378fd87ca7c671cf2f4";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiGeolocationUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(`${apiGeolocationUrl}`).then(showTemperature);
+}
+function getCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+let button = document.querySelector("#current-location-button");
+button.addEventListener("click", getCurrentPosition);
